5adb8b56cee71645c4060b49d75d8d92a6f7fe76
glorl <glorl@gmx.de>
2024-03-07 11:13:22 +0100

    re-init software project

--- +source/voicegen.py
+++ +source/voicegen.py
@@ -1,22 +1,34 @@
 import os
 import sys
+import json
 import re
 
 path_templates = '/home/georg/Dokumente/Quintett/+templates/'
 path_voices = '/home/georg/Dokumente/Quintett/+voices/'
+path_json = '/home/georg/Dokumente/Quintett/+voices' 
 
-# parameters to set in sample file for voice.lytex 
-title = 'Passacaglia'
-composer = 'Bach'
-composer_long ='J.S. Bach, arr. I. Patscheck'
-voice = 'bass'
-paperheight = '#280'
-paperwidth = '#230'
-horizontalshift = '-15mm'
-printpagenumber='##f'
-padding = '5'
-basicdistance = '0.1'
-tf_subtitle = 'false'
+# global parameters
+emptyline = '            \\fill-line {\\line{\\abs-fontsize #30 { \\sans {\\null} }} }'
+paperheight = "#280"
+paperwidth = "#230"
+horizontalshift = "-15mm"
+printpagenumber = "##f"
+
+# specific parameters 
+f = open (os.path.join(path_json,'input_bass.json'), "r")
+data = json.loads(f.read())
+ 
+# Iterating through the json list
+for i in data['Settings']:
+    title = i['title']
+    composer = i['composer']
+    composer_long =i['composer_long']
+    voice = i['voice']
+    padding = i['padding']
+    basicdistance = i['basicdistance']
+    tf_subtitle = i['tf_subtitle']
+    n_emptyline = i['n_emptyline']
+f.close()
 
 f = open(os.path.join(path_templates,'sample.lytex'),"r")
 copy = open(os.path.join(path_voices,composer+'_'+title+'_'+voice+'.lytex'),"wt")
@@ -33,9 +45,14 @@ rep = {"title": title,
 rep = dict((re.escape(k), v) for k, v in rep.items()) 
 pattern = re.compile("|".join(rep.keys()))
 
+#write bookpart.lytex
 for line in f:
     line = pattern.sub(lambda m: rep[re.escape(m.group(0))], line)
-    copy.write(line)
 
+    if line.find('empty')>=0:
+        for kk in range(int(n_emptyline)):
+            copy.write(emptyline+'\n')
+    else: 
+        copy.write(line)
 f.close()
 copy.close()
